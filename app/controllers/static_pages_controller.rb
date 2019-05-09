3b6ad5604c6bdbe64ab8f5f5340cdafdf6e5f232
ziyuqiu <ziyuqiu@brandeis.edu>
2019-05-09 15:03:44 -0400

    update requests

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,11 +4,33 @@ class StaticPagesController < ApplicationController
 
 	def search
   	@params = params
-  	ret = Faraday.get "http://127.0.0.1:5000/"
-  	# , {params}
-  	puts ret.body => {
-  		html: "..."
-  	}
+  	puts params[:key]
+		params[:restaurants] = params[:concern].include? 'Restaurants'
+		params[:gym] = params[:concern].include? 'Gym'
+		params[:laundromat] = params[:concern].include? 'Laundromat'
+		params[:pet] = params[:concern].include? 'Pet'
+		params[:healthcare] = params[:concern].include? 'Healthcare'
+		params[:school] = params[:concern].include? 'School'
+
+		puts params
+  	conn = Faraday.new(:url => 'http://127.0.0.1:5000')
+  	# ret = conn.get "/"
+  	# ret = conn.post "/test_post", {:key => params[:key]}
+  	
+  	msg = "/test_post?company_address=#{params[:address]}
+  	&commute_time_limit=#{params[:commute]}
+  	&means_of_transport=#{{params[:transportation]}}
+  	&safety_concern=#{{params[:safety]}}
+  	&restaurants=#{{params[:restaurants]}}
+  	&gyms=#{{params[:gym]}}
+  	&laundromat=#{{params[:laundromat]}}
+  	&pet=#{{params[:pet]}}
+  	&healthcare=#{{params[:healthcare]}}
+  	&school=#{{params[:school]}}"
+
+  	ret = conn.post msg
+
+  	puts ret.body 
 	end
 
 end
